591d7c632c60606239c67e42a6760d08c71eb921
MatMark <235605@student.pwr.edu.pl>
2019-10-26 15:05:06 +0200

    test

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+import pyodbc
+import json
+
+conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
+                      'Server=w4rta-host.database.windows.net;'
+                      'Database=W4rtaDB;'
+                      'UID=Bird;'
+                      'PWD=roman-IBD;')
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return "Go to /test"
+
+
+@app.route('/test')
+def test():
+    cursor = conn.cursor()
+    data = cursor.execute('SELECT TOP (?) * FROM dbo.ADDRESS;', [1000])
+    rows = data.fetchall()
+    results = []
+    for row in rows:
+        results.append([x for x in row])
+    return json.dumps(results)
+
+
+if __name__ == '__main__':
+    app.run()

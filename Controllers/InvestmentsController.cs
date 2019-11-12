3bd42d85710016197c38695f43fa4bd7f81c4a7f
Marek Morys <mortys1000@gmail.com>
2019-11-12 21:21:43 +0100

    Investments v4

--- Controllers/InvestmentsController.cs
+++ Controllers/InvestmentsController.cs
@@ -20,6 +20,41 @@ namespace BackEnd.Controllers
             investmentsManager = new InvestmentsManager(this.context);
         }
 
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IEnumerable<Investments> investments = investmentsManager.GetAll();
+            return Ok(investments);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            Investments investments = investmentsManager.Get(id);
+            if (investments == null)
+                return NotFound("Investment could not be found");
+            return Ok(investments);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Investments investments)
+        {
+            if (investments == null)
+            {
+                return BadRequest("Address is null");
+            }
+            investmentsManager.Add(investments);
+            return Ok(investments.Id);
+        }
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            Investments investments = investmentsManager.Get(id);
+            if (investments == null)
+                return NotFound("Address couldn't be found");
+            investmentsManager.Delete(investments);
+            return NoContent();
+        }
 
     }
 }

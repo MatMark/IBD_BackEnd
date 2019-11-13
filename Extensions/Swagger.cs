39e27a0b433348fe7e6ab1c12b1f9415c98d8489
MatMark <235605@student.pwr.edu.pl>
2019-11-13 01:20:11 +0100

    Swagger configuration

--- Extensions/Swagger.cs
+++ Extensions/Swagger.cs
@@ -10,11 +10,11 @@ namespace BackEnd.Extensions
 {
     public static class Swagger
     {
-        public static IServiceCollection AddSwag(this IServiceCollection services) //w extension methods zawsze pierwszym parametrem jest rozszerzany typ
-        {                                                                          //ze słówkiem kluczowym "this"
+        public static IServiceCollection AddSwag(this IServiceCollection services)
+        {
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" }); //ctrl c/v z dokumentacji .net core, najbardziej podstawowa konfiguracja generatora dokumentacji
+                c.SwaggerDoc("v1", new Info { Title = "W4rta", Version = "v1" });
             });
 
             return services;
@@ -22,10 +22,10 @@ namespace BackEnd.Extensions
 
         public static IApplicationBuilder UseSwag(this IApplicationBuilder app)
         {
-            app.UseSwagger();       //tutaj dodajemy middleware, które zbierze informacje o API na potrzeby generowania dokumentacji
-            app.UseSwaggerUI(c =>   //tutaj dodajemy middleware, które "postawi" endpoint z gotową dokumentacją (taką, jaką przeklikiwaliśmy na zajęciach)
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                //c.SwaggerEndpoint("/swagger/v1/swagger.json", "W4rta V1");
             });
 
             return app;
